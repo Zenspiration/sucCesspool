b4f607db71e5f1cb0516cca44ba0213017e35c76
luciachen <lucia.chen@yale.edu>
2014-11-28 23:44:12 -0500

    Created CreateCircleActivity.java

---
+++ ParseStarterProject/src/com/parse/starter/CreateCircleActivity.java
@@ -0,0 +1,62 @@
+package com.cpsc112yale.successpoolapp;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.*;
+
+public class CreateCircleActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        //when user clicks "Click me to calculate money per day",
+        	//app takes user input for inputCycle Length and inputMoneyCommitted
+        	//to calculate what each day is worth and to print it out to the screen
+        Button buttonCalculate = (Button)findViewById(R.id.buttonCalculate);      
+        buttonCalculate.setOnClickListener(new View.OnClickListener()
+        {
+        	public void onClick(View v)
+        	{
+        		EditText inputCircleName = (EditText)findViewById(R.id.inputCircleName);
+                EditText inputCycleLength = (EditText)findViewById(R.id.inputCycleLength);
+                EditText inputMoneyCommitted = (EditText)findViewById(R.id.inputMoneyCommitted);
+                
+                double cycleLength = Integer.parseInt(inputCycleLength.getText().toString());
+                double moneyCommitted = Integer.parseInt(inputMoneyCommitted.getText().toString());
+                
+                double moneyPerDay = Math.round(moneyCommitted / cycleLength);
+                
+                //the following line from Stack Overflow rounds the money value to 2 decimal points
+                String moneyPerDayRounded = String.format("%.2f", moneyPerDay);
+                
+                TextView display = (TextView)findViewById(R.id.displayMoneyPerDay);
+                display.setText("Each day is worth $" + moneyPerDayRounded + ".");
+        	}
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
