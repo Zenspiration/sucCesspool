2e25322d1709bb86f6edbaf8f03e28fa497243bc
kaityhsieh <khsieh26@gmail.com>
2014-11-30 20:24:57 -0500

    Added buttons to access all pgs (Kaity)

--- ParseStarterProject/src/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/com/parse/starter/MainActivity.java
@@ -11,7 +11,39 @@ public class MainActivity {
 		goal.put("amountEachMemberContributes", false);
 		goal.put("amountLeftForTheWeek", 8);
 		goal.saveInBackground();
+	
+	    Button viewPoolsButton = (Button) findViewById(R.id.view_pools_button);
+	    viewPoolsButton.setOnClickListener(new OnClickListener() {
+	      public void onClick(View v) {
+	        // Starts an intent for the sign up activity
+	        startActivity(new Intent(MainActivity.this, PoolOverview.class));
+	      }	
 	}
+	
+	    Button createNewButton = (Button) findViewById(R.id.create_pool_button);
+	    createNewButton.setOnClickListener(new OnClickListener() {
+	      public void onClick(View v) {
+	        // Starts an intent for the sign up activity
+	        startActivity(new Intent(MainActivity.this, CreateCircleActivity.class));
+	      }	
+	}
+
+	    Button setGoalsButton = (Button) findViewById(R.id.set_goals_button);
+	    setGoalsButton.setOnClickListener(new OnClickListener() {
+	      public void onClick(View v) {
+	        // Starts an intent for the sign up activity
+	        startActivity(new Intent(MainActivity.this, NewGoalActivity.class));
+	      }	
+	}
+	
+	    Button viewGoalsButton = (Button) findViewById(R.id.view_goals_button);
+	    viewGoalsButton.setOnClickListener(new OnClickListener() {
+	      public void onClick(View v) {
+	        // Starts an intent for the sign up activity
+	        startActivity(new Intent(MainActivity.this, GoalListActivity.class));
+	      }	
+	}
+
 
 }
 
