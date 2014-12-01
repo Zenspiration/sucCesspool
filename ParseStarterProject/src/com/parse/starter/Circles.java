d3003bd2b2cc5804a7a409463f0277c01004100d
kaityhsieh <khsieh26@gmail.com>
2014-11-30 19:33:02 -0500

    Create circles.java (Kaity)
    Creates a new parse class called Circles

---
+++ ParseStarterProject/src/com/parse/starter/Circles.java
@@ -0,0 +1,46 @@
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+@ParseClassName("Pool")
+	  public class Pool extends ParseObject {
+	   
+	 //pulling each of the items & setting them: Pool Title, First User, Goals, (Second User, Third User...).  
+	      public Pool() {
+	          // A default constructor is required.
+	      }
+	   
+	      public String getPoolTitle() {
+	          return getString("poolTitle");
+	      }
+	   
+	      public void setPoolTitle(String title) {
+	          put("title", title);
+	      }
+	   
+	      public ParseUser getFirstUser() {
+	          return getParseUser("firstUser");
+	      }
+	   
+	      public void setFirstUser(ParseUser user) {
+	          put("firstUser", user);
+	      }
+	   
+	      public String getGoals() {
+	          return getString("goals");
+	      }
+	   
+	      public void setGoals(String goals) {
+	          put("goals", goals);
+	      } 
+	      
+	      public int getFirstUsersPoints() {
+	          return getInt("points");
+	      }
+	   
+	      public void setFirstUsersPoints(int points) {
+	          put("points", points);
+	      } 
+
+	}
