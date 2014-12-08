d982cbd55f18ccd98696571d74b9510a9b6ca561
kaityhsieh <khsieh26@gmail.com>
2014-12-08 16:37:43 -0500

    Link to list_item.xml for custom view (Kaity 12-8)

--- ParseStarterProject/src/com/parse/starter/CustomAdapter.java
+++ ParseStarterProject/src/com/parse/starter/CustomAdapter.java
@@ -1,17 +1,13 @@
-package com.parse.starter;
-
-import android.content.Context;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import com.parse.ParseClassName;
 import com.parse.ParseFile;
 import com.parse.ParseImageView;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseQueryAdapter;
 import com.parse.ParseUser;
+import android.view.View;
+import android.widget.TextView;
+import android.view.ViewGroup;
+import android.content.Context;
 
 public class CustomAdapter extends ParseQueryAdapter<Goal> {
 
@@ -30,20 +26,20 @@ static ParseUser currentUser = ParseUser.getCurrentUser();
 	}
 
 	// Customize the layout by overriding getItemView
-//	@Override
-//	public View getItemView(ParseObject object, View v, ViewGroup parent) {
-//		if (v == null) {
-//			v = View.inflate(getContext(), R.layout.activity_goal_list, null);
-//		}
-//
-//		// Add the title view
-//		TextView titleTextView = (TextView) v.findViewById(R.id.text1);
-//		titleTextView.setText(object.getString("title"));
-//
-//		// Add a reminder of how long this item has been outstanding
-//		TextView timestampView = (TextView) v.findViewById(R.id.timestamp);
-//		timestampView.setText(object.getCreatedAt().toString());
-//		return v;
-//	}
+	@Override
+	public View getItemView(Goal goal, View v, ViewGroup parent) {
+		if (v == null) {
+			v = View.inflate(getContext(), R.layout.list_item, null);
+		}
+
+		// Add the title view
+		TextView titleTextView = (TextView) v.findViewById(R.id.text1);
+		titleTextView.setText(goal.getString("title"));
+
+		// Add a reminder of how long this item has been outstanding
+		TextView timestampView = (TextView) v.findViewById(R.id.timestamp);
+		timestampView.setText(goal.getCreatedAt().toString());
+		return v;
+	}
 
 }
