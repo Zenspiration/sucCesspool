70321f4c318aabf4b6d85cc3a8b9ac4f0a507361
luciachen <lucia.chen@yale.edu>
2014-12-10 22:59:38 -0500

    comments for circleDisplayActivity.java and activity_circle_display.xml

--- ParseStarterProject/src/com/parse/starter/CircleDisplayActivity.java
+++ ParseStarterProject/src/com/parse/starter/CircleDisplayActivity.java
@@ -1,3 +1,7 @@
+//CircleDisplayActivity displays information about the pool the user created
+	//pool name, cycle length, money committed, and charity are saved into the Circle class after user inputs them in CreateCircleActivity
+	//CircleDisplayActivity pulls this information from the Circle class and displays them on the "View Your Pool" page
+
 package com.parse.starter;
 
 import java.text.SimpleDateFormat;
@@ -11,11 +15,12 @@ import android.app.Service;
 import android.os.IBinder;
 import android.util.Log;
 
-
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseQueryAdapter;
 import com.parse.ParseUser;
+import com.parse.GetCallback;
+import com.parse.ParseException;
 
 import android.os.Bundle;
 import android.view.Menu;
@@ -25,68 +30,27 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.parse.GetCallback;
-import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
-import com.parse.ParseQueryAdapter;
-import com.parse.ParseUser;
-
-//import android.support.v7.app.ActionBarActivity;
-
 	public class CircleDisplayActivity extends Activity
 	{ 
+		//displays the pool created by the current logged in user
 		ParseUser currentUser = ParseUser.getCurrentUser();
 
-		/*
-		ParseQuery<ParseObject> query = ParseQuery.getQuery("Circle");
-		String inputCircleName = Circle.getString("circleName");
-		double inputMoneyCommitted = Circle.getDouble("dollarsCommitted");
-		String inputCycleLength = Circle.getString("cycleLength");
-		String inputCharity = Circle.getString("charity");
-		*/
-	
-		/*query.getInBackground("xWMyZ4YEGZ", new GetCallback<ParseObject>() {
-		  public void done(ParseObject object, ParseException e) {
-		    if (e == null) {
-@@ -46,9 +42,8 @@ import com.parse.ParseObject;
-		    }
-		  }
-		}); */
-		
-		
-				
-/*		public View getItemView(Circle circle, View v, ViewGroup parent) 
-		{
-			super.getItemView(circle, v, parent);
-			
-@@ -66,12 +61,45 @@ import com.parse.ParseObject;
-	        
-	        return v;			
-		}
-*/
-	
-
-		
 		public void onCreate(Bundle savedInstanceState) 
 		{
 			super.onCreate(savedInstanceState);
-			setContentView(R.layout.activity_circle_display);
+			setContentView(R.layout.activity_circle_display); //sets layout of the page to activity_circle_display.xml
 			
-			
+			//learned how to connect front-end and back-end of displayed user input and buttons from Android Development Tutorial
 			final TextView inputCircleName = (TextView)findViewById(R.id.inputCircleName);
 			final TextView inputCycleLength = (TextView)findViewById(R.id.inputCycleLength);
 			final TextView inputDollarsCommitted = (TextView)findViewById(R.id.inputMoneyCommitted);
 			final TextView inputCharity = (TextView)findViewById(R.id.inputCharity);
 			final TextView timeRemaining= (TextView) findViewById(R.id.timeRemaining);
-			final TextView inputMoneyGained = (TextView) findViewById(R.id.inputMoneyGained);
-			final TextView inputDollarsCommitted2 = (TextView) findViewById(R.id.inputMoneyCommitted2);
 			
 			final Button btnStart = (Button) findViewById(R.id.buttonSetGoals);
 		    final Intent serviceIntent = new Intent(CircleDisplayActivity.this, MyService.class);
-
 			
-			
+		    //learned how to set up a ParseQuery from Parse Android tutorial
 	    	ParseQuery<Circle> query = ParseQuery.getQuery("Circle");	    	
 	    	query.whereEqualTo("userId", currentUser.getObjectId());
 	    	query.whereEqualTo("archive", false);
@@ -97,21 +61,19 @@ import com.parse.ParseUser;
 		    	{
 		    		if (e == null) 
 		    		{
+		    			//gets pool name, cycle length, money committed, and charity name from the Circle class
 		    			String circleName = circle.getString("name");
 	    	            int cycleLength = circle.getInt("cycleLength");
 	    	            int dollarsCommitted = circle.getInt("dollars");
 		    	        String charity = circle.getString("charity");
-		    	        //int moneyGained = circle.getInt("moneyGained"); //Jarrell you can add the moneyGained variable into Circle.java
 		    	        
+		    	        //sets textview of relevant variables in activity_circle_display.xml to what was pulled from the Circle class
 		    	     	inputCircleName.setText(circleName);
 		    	     	inputCycleLength.setText("" + cycleLength);
 		    	     	inputDollarsCommitted.setText("" + dollarsCommitted);
-		    	     	inputCharity.setText(charity);
-		    	     	//inputMoneyGained.setText("" + moneyGained); //Jarrell you can add the moneyGained variable into Circle.java
-		    	     	inputDollarsCommitted2.setText("" + dollarsCommitted);
-		    		    
+		    	     	inputCharity.setText(charity);		    		    
 		    	     	
-		    	     	//sets up a timer
+		    	     //sets up a timer
 		    	     	
 		    	       /*int millisecondsInCycle=cycleLength*24*60*60*1000;
 		    	    	CountDownTimer aCounter = new CountDownTimer(millisecondsInCycle , 1000) {
@@ -137,58 +99,22 @@ import com.parse.ParseUser;
 		    	     	//serviceIntent = new Intent(CircleDisplayActivity.this, MyService.class);
 
 		    	     	//btnStart.setOnClickListener(new View.OnClickListener() {
-
-		    	            //@Override
-		    	          //  public void onClick(View v) {
-		    	                // TODO Auto-generated method stub
-		    	                
-		    	            //}
 		    	        //});
 		    	    }
-	
 		    	}
-		    	
 	    	});
-	    	
 	
-    Button buttonSetGoals = (Button)findViewById(R.id.buttonSetGoals);
-    buttonSetGoals.setOnClickListener(new View.OnClickListener() {
-    	public void onClick(View v)
-    	{
-    		Intent intent = new Intent(CircleDisplayActivity.this, GoalListActivity.class);
-    		startActivity(intent);
-    		startService(serviceIntent);
-    	}
-	});
+	//creates the "View Your Goals" button that links to the front-end layout for the button in activity_circle_display
+    Button buttonViewGoals = (Button)findViewById(R.id.buttonSetGoals);
+    //when button is clicked, sets up an intent that takes the user to GoalListActivity
+    buttonViewGoals.setOnClickListener(new View.OnClickListener() {
+	    	public void onClick(View v)
+	    	{
+	    		Intent intent = new Intent(CircleDisplayActivity.this, GoalListActivity.class);
+	    		startActivity(intent);
+	    		startService(serviceIntent);
+	    	}
+		});
 	}
 }
-
-	
-
-
-/*			mainAdapter = new ParseQueryAdapter<Circle>(this, Circle.class);
-			//mainAdapter = new ParseQueryAdapter<ParseObject>(this, "Todo");
-
-			mainAdapter.setTextKey("circle name");
-@@ -83,40 +111,15 @@ import com.parse.ParseObject;
-			
-			//getListView().setClickable(false);
-			 			
-		} */
-			
-/*		@Override
-		public boolean onCreateOptionsMenu(Menu menu) {
-			getMenuInflater().inflate(R.menu.activity_pool_list, menu);
-			return true;
-		}
-			 
-		@Override
-		public boolean onOptionsItemSelected(MenuItem item) {
-			switch (item.getItemId()) {
-	
-@@ -128,6 +131,8 @@ import com.parse.ParseObject;
-			}
-			return super.onOptionsItemSelected(item);
-		}
-	*/
 
